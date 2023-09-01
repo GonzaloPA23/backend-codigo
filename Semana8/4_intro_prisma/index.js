d9189a7f6f4663784e048b1ab89e784635484a34
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-08-31 23:03:28 -0500

    Metodos get y post con Express y nodejs - conexion base de datos

--- Semana8/4_intro_prisma/index.js
+++ Semana8/4_intro_prisma/index.js
@@ -1,8 +1,41 @@
 import  express from "express";
+import  Prisma  from "@prisma/client";
+
+const conexion = new Prisma.PrismaClient();
 
 const servidor = express();
 
+servidor.use(express.json());
+
 const PORT = 3000;
+
+servidor.route("/grados").post(async (req,res)=>{
+    const {body:data}  = req; // cuerpo de la peticion  // destructuracion de objetos req.body ahora sera data
+    try {
+        const resultado = await conexion.grado.create({
+            data,
+            // {
+            //     nombreNumerico: body.nombreNumerico,
+            //     nombreTexto: body.nombreTexto,
+            // }
+        })
+        console.log(resultado);
+        res.json({
+            message: "Grado creado exitosamente",
+        }), 201;
+    } catch (error) {
+        res.json({
+            message: "Error al crear el grado",
+        }); 400;            
+    }
+}).get(async(req,res)=>{
+    const resultado = await conexion.grado.findMany();
+
+    res.json({
+        content: resultado,
+    })
+})
+
 
 servidor.listen(PORT,()=>{
     console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`)
